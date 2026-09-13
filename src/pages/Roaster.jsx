1da35fc2348c24d7c9abfc6a4d9cf127dc13776a
hewyxy <hewyxy@gmail.com>
2026-09-13 15:21:47 -0500

    Roaster page now idetifys players team

--- src/pages/Roaster.jsx
+++ src/pages/Roaster.jsx
@@ -1,12 +1,24 @@
 
-import React from "react";
 import CurrentRoaster from "../components/CurrentRoaster";
+import React, { useEffect, useState } from "react";
 
 export default function Roaster() {
+
+    const [team, setTeam] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/api/teams/1")
+            .then(response => response.json())
+            .then(data => setTeam(data))
+            .catch(error => console.error("Error fetching team:", error));
+    }, []);
+
+    const teamName = team.Name || "";
+
     return (
         <div>
             <CurrentRoaster
-                teamName="Spirit"
+                teamName="Falcons"
             />
         </div>  
     );
