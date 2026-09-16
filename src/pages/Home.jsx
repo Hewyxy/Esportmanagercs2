445804ee25012b0d82c221b57d72b796f355da32
hewyxy <hewyxy@gmail.com>
2026-09-15 20:13:51 -0500

    Database was updated

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
         <div className="page">
             <h1>Tournaments</h1>
             <div className="tournament-container">
-                <Tournament name="Esports World Cup" image="/path/to/image1.jpg" prize="$2,000,000" />
-                <Tournament name="PGL Major" image="/path/to/image2.jpg" prize="$5,000,000" />
-                <Tournament name="BLAST Premier" image="/path/to/image3.jpg" prize="$2,500,000" />
+                <Tournament name="Esports World Cup" image="/src/assets/EWC.png" prize="$2,000,000" />
+                <Tournament name="PGL Major" image="/src/assets/PGL.png" prize="$5,000,000" />
+                <Tournament name="BLAST Premier" image="/src/assets/BLAST.png" prize="$2,500,000" />
             </div>
             
         </div>
